c828e66de2a05d26a22b93e87a8427b656621bd1
Savior Joseph Jacob <aziptechinc@gmail.com>
2025-09-19 18:35:59 +0000

    Now let's implement app into a progressive web application using the log

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,9 +36,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Pentecostal Church of the Living God</title>
-        <meta name="description" content="A place for faith-based connection and growth." />
+        <meta name="description" content="A place for faith-based connection, growth, and community engagement." />
+        <meta name="keywords" content="church, pentecostal, community, faith, God, Jesus, Bible, worship, PCOTLG" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/images/logo.png"></link>
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={cn("font-body antialiased", inter.variable, notoSerif.variable, ebGaramond.variable)}>
         <ThemeProvider
